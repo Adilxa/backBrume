10d333fbe5f761f339e560bb0e921de15ad399ee
Adilxa <adilhanimerov82@gmail.com>
2025-08-24 15:23:33 +0600

    cors

--- index.js
+++ index.js
@@ -6,35 +6,9 @@ import swaggerSpecs from "./swagger.js";
 import routes from "./src/routes/index.js";
 
 const corsOptions = {
-  origin: [
-    "http://localhost:3000", // React default
-    "http://localhost:3001",
-    "http://localhost:4200", // Angular default
-    "http://localhost:8080", // Vue default
-
-    // Backend порты для тестирования
-    "http://localhost:5000",
-    "http://localhost:8000",
-
-    // Альтернативные хосты
-    "http://127.0.0.1:3000",
-    "http://127.0.0.1:3001",
-
-    // Продакшен домены
-    "https://brume.kg/",
-    "https://admin.brume.kg/",
-  ],
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
-  allowedHeaders: [
-    "Origin",
-    "X-Requested-With",
-    "Content-Type",
-    "Accept",
-    "Authorization",
-    "Cache-Control",
-  ],
+  origin: true, // Разрешить все origins
   credentials: true,
-  optionsSuccessStatus: 200, // для поддержки старых браузеров
+  optionsSuccessStatus: 200,
 };
 
 dotenv.config();
